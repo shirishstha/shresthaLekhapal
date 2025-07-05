f0784166c966077df5a19ef199c4f91dc734e23e
shirishstha <sthasiriz123@gmail.com>
2025-07-05 18:37:10 +0545

    updated the login form

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Provider store={store}>
+      <Toaster/>
       <App />
     </Provider>
   </BrowserRouter>
